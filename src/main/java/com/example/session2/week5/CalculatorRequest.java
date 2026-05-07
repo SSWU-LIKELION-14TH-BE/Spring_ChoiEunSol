12d533a2028ed30731a569b4547a20f1e965dea2
eunsol <cindychoi03@naver.com>
2026-05-08 03:55:56 +0900

    refactor: CalculatorRequest 수정 및 설정 파일 정리

--- src/main/java/com/example/session2/week5/CalculatorRequest.java
+++ src/main/java/com/example/session2/week5/CalculatorRequest.java
@@ -1,10 +1,9 @@
 package com.example.session2.week5;
 
 public class CalculatorRequest {
-    private int number1;
-    private int number2;
+    private final int number1;
+    private final int number2;
 
-    public CalculatorRequest() {}
     // 생성자
     public CalculatorRequest(int number1, int number2) {
         this.number1 = number1;
